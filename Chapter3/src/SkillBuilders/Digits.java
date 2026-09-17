811cde5f17496450986e7c21e7484eb887f6735c
510155001 <510155001@L815WD220028.edu.cbe.ab.ca>
2026-09-17 09:04:08 -0600

    9:04 AM

---
+++ Chapter3/src/SkillBuilders/Digits.java
@@ -0,0 +1,61 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class Digits {
+
+    public static void main(String[] args) {
+
+        // Create A Scanner Object
+        Scanner userinput = new Scanner(System.in);
+
+        // Ask for number
+        System.out.print("Please enter a number: ");
+
+        int Num = 0;
+
+        // Loop until a positive numerical value is given
+        while (true) {
+
+            try {
+
+                // Get input
+                Num = userinput.nextInt();
+
+                // Check if number is positive
+                if (Num >= 0)
+                    break;
+
+                // If not positive, display error message and loop
+                System.out.println("Error: Please enter a positive value");
+
+            }
+
+            // If a non-numerical value is presented, display error message and loop
+            catch (Exception e) {
+
+                System.out.println("Error: Please enter a numerical value.");
+
+                if (userinput.hasNext()) {
+                    userinput.next();
+                }
+            }
+        }
+
+        String numString = String.valueOf(Num);
+
+        // Find length
+        int length = numString.length();
+
+        // Loop for length of the number
+        for (int i = 0; i < length; i++) {
+
+            int digit = Character.getNumericValue(numString.charAt(i));
+
+            System.out.println(digit + " x "
+                    + Math.pow(10, length - i - 1)
+                    + " = "
+                    + (digit * Math.pow(10, length - i - 1)));
+        }
+    }
+}
