998d70f5d31696ade21fde614924e3437e30c2ea
510155001 <510155001@L815WD220028.edu.cbe.ab.ca>
2026-09-17 10:00:09 -0600

    10:00 AM

---
+++ Chapter3/src/SkillBuilders/CompoundIntrest.java
@@ -0,0 +1,61 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class CompoundIntrest
+{
+    // Define a new function
+	public static float GetNumber(Scanner userinput)
+    {
+        // Loop until a positive numerical value is given
+		while (true)
+        {
+            try
+            {
+            	// Get input
+                float number = userinput.nextFloat();
+
+                // Check if number is positive
+                if (number >= 0)
+                	return number;
+
+                // If not positive, display error message and loop
+                System.out.println("Error: Please enter a positive value");
+            }
+            // If a non- numerical value is presented, display error message and loop
+            catch (Exception e)
+            {
+                System.out.println("Error: Please enter a numerical value.");
+
+                if (userinput.hasNext())
+                {
+                    userinput.next();
+                }
+            }
+        }
+    }
+
+	// Run main code
+    public static void main(String[] args)
+    {
+        // Create A Scanner Object
+        Scanner userinput = new Scanner(System.in);
+
+        System.out.print("Enter the principal ($): ");
+        float principal = GetNumber(userinput);
+        
+        System.out.print("Enter the period (months): ");
+        float months = GetNumber(userinput) / 12;
+        
+        System.out.print("Enter the intrest rate (%): ");
+        float rate = GetNumber(userinput) / 100;
+        
+        System.out.print("Enter the compounding frequency (days): ");
+        float freq = 365 / GetNumber(userinput);
+
+        float total = (float) (principal * Math.pow(1 + rate / freq, freq * months));
+        
+        // Display output
+        System.out.printf("The value after the term is $%.2f", total);
+    }
+}
